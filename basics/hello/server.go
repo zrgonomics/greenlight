36d403ce394ec2c4b8a3088915ffc87311098c94
zrgonomics <193090173+zrgonomics@users.noreply.github.com>
2025-12-07 17:23:28 -0500

    Add Server struct and internalCache

--- basics/hello/server.go
+++ basics/hello/server.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+type Server struct {
+	Port int8
+}
+
+var internalCache = make(map[string]string)
+
 func StartServer() { // TitleCase name = public, exported
 	fmt.Println("Starting server...")
 	helper()
